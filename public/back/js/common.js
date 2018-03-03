f938f9cdfccd57dd214256151500eb383d8e3f24
chenxianping <1006877475@qq.com>
2018-03-03 17:07:53 +0800

    进度条插件功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,23 @@
 /**
  * Created by Administrator on 2018/3/3.
  */
+$(function(){
+    //1、加载进度条插件
+    //禁用进度环
+    NProgress.configure({
+        showSpinner: false
+    })
+
+    //发送ajax请求开始时加载进度条
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    });
+
+    //ajax请求结束后关闭进度条
+    $(document).ajaxStop(function () {
+        setTimeout(function(){
+            NProgress.done();
+        },500)
+    })
+})
+
